031b4efdfbdec1335919685c6c8b71c324cc00bb
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-08-15 11:08:44 +0530

    Create variable.c

---
+++ variable.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	int a; // if we donot initialise the variable then the default value is assigned is 0 and a memory is also allocated to that variable
+    printf("%d",a);
+    return 0;
+}
