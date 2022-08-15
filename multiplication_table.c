d17ff9143bb988632df4dff59848170664d08869
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-08-15 11:52:43 +0530

    Create multiplication_table.c

---
+++ multiplication_table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Enter the number\n");
+	scanf("%d",&a);
+	printf("%d * %d = %d\n",a,1,a*1);
+	printf("%d * %d = %d\n",a,2,a*2);
+	printf("%d * %d = %d\n",a,3,a*3);
+	printf("%d * %d = %d\n",a,4,a*4);
+	printf("%d * %d = %d\n",a,5,a*5);
+	printf("%d * %d = %d\n",a,6,a*6);
+	printf("%d * %d = %d\n",a,7,a*7);
+	printf("%d * %d = %d\n",a,8,a*8);
+	printf("%d * %d = %d\n",a,9,a*9);
+	printf("%d * %d = %d\n",a,10,a*10);
+	
+}
