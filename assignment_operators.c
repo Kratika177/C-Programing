a553965f4e16a593f66a96b639651bf15e34eb67
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-08-15 11:42:36 +0530

    Create assignment_operators.c

---
+++ assignment_operators.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int a=10;
+	int b=20;
+    printf("a + b = %d\n",a+=b,"\n");
+    printf("a * b = %d\n",a*=b,"\n");
+    printf("b - a = %d\n",b-=a,"\n");
+    return 0;
+}
